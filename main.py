7e16c3d6b20fbbe2de0973b74674f2f205543956
up-44 <155664923+up-44@users.noreply.github.com>
2025-11-02 14:21:00 +0100

    test code funktioniert

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import dht # type: ignore
-from machine import Pin # type: ignore
+import dht  # type: ignore
+from machine import Pin  # type: ignore
 import time
 
-# Sensor an GPIO4, DHT11
-sensor = dht.DHT11(Pin(4))
+# DHT22 Sensor an GPIO 2
+sensor = dht.DHT22(Pin(2))  # type: ignore
 
 while True:
     try:
         sensor.measure()
         temp = sensor.temperature()
-        hum = sensor.humidity()
-        print(f"Temperatur: {temp} °C, Luftfeuchtigkeit: {hum} %")
+        hum = sensor.humidity() 
+        print(f"Temperatur: {temp:.1f} °C, Luftfeuchtigkeit: {hum:.1f} %")
     except OSError as e:
-        print("Fehler beim Lesen des Sensors:", e)
-    time.sleep(2)
+        print("Fehler beim Auslesen des Sensors:", e)
+    time.sleep(2)  # alle 2 Sekunden messen
